63c33162e197f0b1d1cff58d9fda549814a32b03
Juan Arillo <juanarillo@outlook.com>
2017-06-05 15:59:15 +0200

    Added __init__.py and __manifest.py files

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'To-Do tasks',
+    'decription': 'Aplicación de listas To-Do',
+    'summary': 'To_Do tasks',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'author': 'Juan Arillo',
+    'website': 'https://github.com/juarru/todo_tasks',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
