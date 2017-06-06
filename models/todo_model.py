dba133871a55dd989ed4935e152aec52e693cfea
Juan Arillo <juanarillo@outlook.com>
2017-06-06 17:34:01 +0200

    Minimun changes into todo_model.py

--- models/todo_model.py
+++ models/todo_model.py
@@ -4,9 +4,9 @@ from odoo import models, fields
 
 class TodoTask(models.Model):
 
-    _name: 'todo.task'
-    _description: 'To-do Task'
+    _name = 'todo.task'
+    _description = 'To-do Task'
 
-    name = fields.Char(string='Description', required= True)
+    name = fields.Char(string='Description', required=True)
     is_done = fields.Boolean(string='Done')
-    active = fields.Boolean(string='Active?', default= True)
+    active = fields.Boolean(string='Active?', default=True)
